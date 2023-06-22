e6871a3e196df863b029512743579e860eb35a7b
IlyaPushkarskiy <p_ilya_007@mail.ru>
2023-06-22 12:19:15 +0300

    Закончил module4-task1

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,18 @@
-```js
 /**
  * Функция для проверки длины строки
  * @param {string} string - исходная строка
  * @param {int} maxLength - длина для проверки
  * @param {boolean} - возвращает истину в случае если длина string меньше значения maxLength
  */
-```
 const isCheckingLength = (string, maxLength) => string.length <= maxLength;
 
 isCheckingLength('строка', 10);
 
-```js
 /**
  * Функция для проверки, является ли строка палиндромом
  * @param {string} example - исходная строка
  * @param {boolean} - возвращает истину в случае если строка является полиндромом
  */
-```
 function isPolindrom(example) {
   const string = example.replaceAll(' ', '').toUpperCase();
   for (let i = 0; i < string.length / 2; i++) {
@@ -26,13 +22,11 @@ function isPolindrom(example) {
 
 isPolindrom('121     121');
 
-```js
 /**
  * Функция для извлечения цифр из строки
  * @param {string} arg - исходная строка
  * @param {int}  parseInt - возвращает цифры из заданной строки arg
  */
-```
 function getNumberOfString(arg) {
   const string = arg.toString();
   let result = '';
