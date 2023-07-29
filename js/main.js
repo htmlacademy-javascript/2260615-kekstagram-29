242e86ae6acbf85ff511ba56291788fca9eb60e3
IlyaPushkarskiy <p_ilya_007@mail.ru>
2023-07-29 11:09:53 +0300

    убрал полоску глубины в default

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-//import { getAllPhotoByUsers } from './data.js';
 import { renderGallery } from './gallery.js';
 import { addHandlerToListener, addHandlerToForm } from './form.js';
 import { getData } from './load.js';
 import { showAlert } from './message.js';
 
 getData(renderGallery, showAlert);
+
 addHandlerToForm();
+
 addHandlerToListener();
