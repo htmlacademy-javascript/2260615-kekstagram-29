be83410f0ce123c59b1acac1fd439d55b45d798e
Keks <keks@htmlacademy.ru>
2023-08-01 13:17:58 +0300

    :heavy_check_mark: Сборка #13

--- 13/js/message.js
+++ 13/js/message.js
@@ -1,11 +1,11 @@
+import { isEscapeKey } from './util.js';
 const TIME_OF_SHOW_ALERT = 5000;
-const TypeOfMessage = {
-  SUCCESS: '#008000',
-  ERROR: '#ff4e4e',
-};
 
-// showAllert функция
-const showAlert = (message, type = TypeOfMessage.ERROR) => {
+const errorMessage = document.querySelector('#error').content.querySelector('.error');
+const successMessage = document.querySelector('#success').content.querySelector('.success');
+
+// функция для сообщения о неудачной загрузки
+const showAlert = (message) => {
   const alert = document.createElement('div');
   alert.style.position = 'absolute';
   alert.style.zIndex = '100';
@@ -14,7 +14,7 @@ const showAlert = (message, type = TypeOfMessage.ERROR) => {
   alert.style.right = '0';
   alert.style.padding = '20px 3px';
   alert.style.textAlign = 'center';
-  alert.style.backgroundColor = type;
+  alert.style.backgroundColor = '#ff4e4e';
   alert.style.fontSize = '20px';
   alert.textContent = message;
   document.body.append(alert);
@@ -24,14 +24,46 @@ const showAlert = (message, type = TypeOfMessage.ERROR) => {
   }, TIME_OF_SHOW_ALERT);
 };
 
-
-//сообщение об успешной или неудачной отправки формы
-const showSuccessMessage = () => {
-  showAlert('Успешно отправлено', TypeOfMessage.SUCCESS);
+//функция закрытия на кнопку esc
+const onMessageEscape = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeMessage();
+  }
 };
 
-const showErrorMessage = () =>{
-  showAlert('Произошла ошибка');
+//функция закрытия на клик вне поля
+const onBodyClick = (evt) => {
+  if (evt.target === document.querySelector('.error, .success')) {
+    evt.preventDefault();
+    closeMessage();
+  }
+};
+
+//функция закрытия формы
+function closeMessage () {
+  const messageElement = document.querySelector('.error, .success');
+  messageElement.remove();
+  document.addEventListener('keydown', onMessageEscape);
+  document.removeEventListener('click', onBodyClick);
+}
+
+//функция отправки формы
+const showMessage = (messageElement, closeButtonClass) => {
+  document.body.append(messageElement);
+  document.addEventListener('click', onBodyClick);
+  document.addEventListener('keydown', onMessageEscape);
+  messageElement.querySelector(closeButtonClass).addEventListener('click', closeMessage);
+};
+
+//сообщение об успешной отправки формы
+const showSuccessMessage = () => {
+  showMessage(successMessage, '.success__button');
+};
+
+//сообщение об ошибки при отправки формы
+const showErrorMessage = () => {
+  showMessage(errorMessage, '.error__button');
 };
 
 export {showSuccessMessage, showErrorMessage, showAlert};
--- 13/js/util.js
+++ 13/js/util.js
@@ -1,15 +1,4 @@
-/**
-  * Функция для создания случайного числа в диапозоне от а до b
-  * @param {int} a - нижняя граница диапозона
-  * @param {int} b - верхняя граница диапозона
-  * @param {int} result - возвращает случайное число в диапозоне от а до b
- */
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const debounce = (callback, timeoutDelay = 500) => {
   let timeoutID;
@@ -19,4 +8,4 @@ const debounce = (callback, timeoutDelay = 500) => {
   };
 };
 
-export {getRandomInteger, debounce};
+export {debounce, isEscapeKey};
